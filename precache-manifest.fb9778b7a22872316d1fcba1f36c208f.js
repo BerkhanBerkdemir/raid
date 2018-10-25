4623c77a57278c87bfb8030f9b83deae94b82cf2
Berkhan Berkdemir <24360355+BerkhanBerkdemir@users.noreply.github.com>
2018-10-25 16:36:56 -0700

    Updates

---
+++ precache-manifest.fb9778b7a22872316d1fcba1f36c208f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e23010e33e3dd07e977b",
+    "url": "/raid/static/js/runtime~main.e23010e3.js"
+  },
+  {
+    "revision": "54aa67fdc17a6300fff3",
+    "url": "/raid/static/js/main.54aa67fd.chunk.js"
+  },
+  {
+    "revision": "7e417a777a4d86431b2a",
+    "url": "/raid/static/js/1.7e417a77.chunk.js"
+  },
+  {
+    "revision": "54aa67fdc17a6300fff3",
+    "url": "/raid/static/css/main.7b6ef8cb.chunk.css"
+  },
+  {
+    "revision": "e7461996a95881528caee8b67bc847e9",
+    "url": "/raid/index.html"
+  }
+];
