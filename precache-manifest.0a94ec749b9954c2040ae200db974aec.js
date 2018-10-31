ab4c21a1d3d0b1b03a9b790e0bbf45cfef429920
Berkhan Berkdemir <24360355+BerkhanBerkdemir@users.noreply.github.com>
2018-10-31 14:58:56 -0700

    Updates

--- precache-manifest.0a94ec749b9954c2040ae200db974aec.js
+++ precache-manifest.0a94ec749b9954c2040ae200db974aec.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/raid/static/js/runtime~main.e23010e3.js"
   },
   {
-    "revision": "54aa67fdc17a6300fff3",
-    "url": "/raid/static/js/main.54aa67fd.chunk.js"
+    "revision": "8c90961f6e76c178fd1f",
+    "url": "/raid/static/js/main.8c90961f.chunk.js"
   },
   {
     "revision": "7e417a777a4d86431b2a",
     "url": "/raid/static/js/1.7e417a77.chunk.js"
   },
   {
-    "revision": "54aa67fdc17a6300fff3",
-    "url": "/raid/static/css/main.7b6ef8cb.chunk.css"
+    "revision": "8c90961f6e76c178fd1f",
+    "url": "/raid/static/css/main.50252661.chunk.css"
   },
   {
-    "revision": "e7461996a95881528caee8b67bc847e9",
+    "revision": "c7d740f04ac7bc5281122ad0e0f0eb83",
     "url": "/raid/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -11,10 +11,10 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/raid/precache-manifest.fb9778b7a22872316d1fcba1f36c208f.js"
+  "/raid/precache-manifest.0a94ec749b9954c2040ae200db974aec.js"
 );
 
 workbox.clientsClaim();
